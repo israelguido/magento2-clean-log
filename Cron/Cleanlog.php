0df6195a1eaeb2563510f2604782f0edddcd0720
israelguido <israelguido@gmail.com>
2020-07-28 21:24:21 -0300

    first commit

---
+++ Cron/Cleanlog.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ *  script by israelguido@gmail.com
+ */
+namespace Danone\CleanLog\Cron;
+
+use Psr\Log\LoggerInterface;
+
+class Cleanlog
+{
+    protected $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+    
+    public function execute()
+    {
+        $this->logger->info('Db Log cleaning cron started..!');
+        $objectManager  = \Magento\Framework\App\ObjectManager::getInstance();
+        $resource       = $objectManager->get('Magento\Framework\App\ResourceConnection');
+        $connection     = $resource->getConnection();
+
+        $tablesToTurncate = [
+            'report_event',
+            'report_viewed_product_index',
+            'report_compared_product_index',
+            'customer_visitor'
+        ];
+        foreach ($tablesToTurncate as $_key => $value) {
+            $tableName  = $resource->getTableName($value);
+            $sql        = "TRUNCATE " . $tableName;
+
+            try {
+                $connection->query($sql);
+                $this->logger->info($tableName . ' cleaned up.');
+            } catch (\Exception $e) {
+                $this->logger->critical($e);
+            }
+        }
+        $this->logger->info('Db Log cleaning cron ended..!');
+
+        return $this;
+    }
+}
